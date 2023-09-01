2f7f346e82d9ca61b14b0e44eea5a7136c4c09f4
Santiago Varacca <santiagovaracca@gmail.com>
2023-09-01 20:35:39 -0300

    create board, load images

---
+++ chess-vuejs/src/components/ChessPiece.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps(["imageSrc"]);
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-full w-full">
+    <img draggable="false" width="60" height="60" :src="props.imageSrc" />
+  </div>
+</template>
